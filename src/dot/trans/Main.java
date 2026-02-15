f039dcad1133dc84d747fba44484467d094e52c7
arseniji <odiousguyfromnet@gmail.com>
2026-02-15 22:36:17 +0300

    dont mind

--- src/dot/trans/Main.java
+++ src/dot/trans/Main.java
@@ -1,10 +1,9 @@
 package dot.trans;
 import dot.trans.lex_analyser.*;
-import dot.trans.rpn_converter.LexifiedArrayFormatter;
-import dot.trans.rpn_converter.RPNStack;
+import dot.trans.rpn.RPN;
+import dot.trans.rpn.SyntaxDefiner;
 
 import java.io.IOException;
-import java.util.List;
 
 
 public class Main {
@@ -16,7 +15,10 @@ public class Main {
             Tokenizer tokenizer = new Tokenizer(content);
             TokenList tokenList = tokenizer.varTokenize();
             tokenList.show();
-
+            SyntaxDefiner sd = new SyntaxDefiner(tokenList);
+            TokenList tkl = sd.processList();
+            System.out.println();
+            tkl.show();
         }
         catch (IOException e) {
             System.err.println("Ошибка чтения файла: " + e.getMessage());
