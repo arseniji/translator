7da60cf5ae12af596bc402f8058a2f454f2e84c3
arseniji <odiousguyfromnet@gmail.com>
2026-02-16 23:48:31 +0300

    hz

--- src/dot/trans/plusFiles/simple.cpp
+++ src/dot/trans/plusFiles/simple.cpp
@@ -1,1 +1,22 @@
-f(ad + 23,df == 1,fa * 32)
+arr[0] = 5;
+x = arr[i];
+matrix[i][j] = 10;
+x = matrix[0][1];
+
+for (int i = 0; i < 10; i++) {
+    arr[i] = i * 2;
+}
+
+x = a + b * c - d / e;
+y = (a + b) * (c - d);
+z = a * b + c * d;
+
+if (x > 5 && y < 10) cout << "yes";
+if (x == 5 || y == 10) cout << "yes";
+if (!(x > 5)) cout << "no";
+
+x++;
+++x;
+x--;
+--x;
+y = -x;
