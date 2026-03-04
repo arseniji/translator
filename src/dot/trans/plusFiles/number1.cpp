19a79aa72a0424ad66e57464ec3adea9ec6fa9c3
arseniji <odiousguyfromnet@gmail.com>
2026-03-04 15:50:19 +0300

    тестирую по мелочи

--- src/dot/trans/plusFiles/number1.cpp
+++ src/dot/trans/plusFiles/number1.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
 int main() {
     int number;
     bool isPrime = true;
--- src/dot/trans/plusFiles/simple.cpp
+++ src/dot/trans/plusFiles/simple.cpp
@@ -1,22 +1,108 @@
-arr[0] = 5;
-x = arr[i];
-matrix[i][j] = 10;
-x = matrix[0][1];
-
-for (int i = 0; i < 10; i++) {
-    arr[i] = i * 2;
+int i = 0;
+int j = 0;
+while (i < 5) {
+    j = 0;
+    while (j < 5) {
+        if (j == 3) break;
+        j = j + 1;
+    }
+    if (i == 2) continue;
+    i = i + 1;
 }
 
-x = a + b * c - d / e;
-y = (a + b) * (c - d);
-z = a * b + c * d;
+int sum = 0;
+for (int i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+        sum = sum + i;
+    } else {
+        sum = sum - 1;
+    }
+}
 
-if (x > 5 && y < 10) cout << "yes";
-if (x == 5 || y == 10) cout << "yes";
-if (!(x > 5)) cout << "no";
+int x = 5;
+if (x == 1) {
+    x = 10;
+} else if (x == 2) {
+    x = 20;
+} else if (x == 3) {
+    x = 30;
+} else if (x == 4) {
+    x = 40;
+} else {
+    x = 99;
+}
 
-x++;
-++x;
-x--;
---x;
-y = -x;
+int n = 1;
+do {
+    n = n * 2;
+    if (n == 8) break;
+} while (n < 100);
+
+int a = 3;
+int b = 5;
+if (a > 0)
+    if (b > 0)
+        a = a + b;
+    else
+        a = 0;
+else
+    b = 0;
+
+    int max(int a, int b) {
+        if (a > b) return a;
+        else return b;
+    }
+
+    int main() {
+        int x = 10;
+        int y = 20;
+        int result = max(x, y);
+        cout << result;
+        return 0;
+    }
+
+    int main() {
+        int arr[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = i * 2;
+        }
+        cout << arr[3];
+        return 0;
+    }
+
+    int a = 2;
+    int b = 3;
+    int c = 4;
+    int result = a + b * c - (a + b) * c / 2;
+    bool flag = a > 0 && b < 10 || c == 4;
+
+    int main() {
+        int a;
+        int b;
+        cin >> a >> b;
+        int sum = a + b;
+        cout << "Sum: " << sum << endl;
+        return 0;
+    }
+
+    int factorial(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    int main() {
+        int n;
+        cin >> n;
+        if (n < 0) {
+            cout << "Error" << endl;
+        } else if (n == 0) {
+            cout << 1 << endl;
+        } else {
+            int f = factorial(n);
+            cout << f << endl;
+        }
+        return 0;
+    }
